2b24d7c505004dc73c65eb14937b36f7d6e8a6ee
QdayAmith <amithtsdev@gmail.com>
2026-06-06 13:39:10 -0400

    add view expenses with formatted output

---
+++ expenses.py
@@ -0,0 +1,42 @@
+import json
+
+try:
+    with open("data/expenses.json", "r") as file:
+        collection_entries = json.load(file)
+except FileNotFoundError:
+    collection_entries = []
+
+def add_expense():
+    user_entries = {
+        "amount" : float(input("Enter the amount to track your expenses:")),
+        "category" : input("Enter the category of your expense:"),
+        "description" : input("Enter the description of your expense :"),
+        "date" : input("Enter the date of your expense (YYYY-MM-DD):")
+    }
+    collection_entries.append(user_entries)
+
+    with open("data/expenses.json", "w") as file:
+        json.dump(collection_entries, file)
+
+    print("Expense added successfully!")
+
+add_expense()
+
+
+def view_expenses():
+    try:
+        with open("data/expenses.json", "r") as file:
+            collection_entries = json.load(file)
+    except FileNotFoundError:
+        collection_entries = []
+
+    for entry in collection_entries:
+        print("-" * 30)
+        print(f"Amount: {entry['amount']}")
+        print(f"Category: {entry['category']}")
+        print(f"Description: {entry['description']}")
+        print(f"Date: {entry['date']}")
+
+    print("-" * 30)
+
+view_expenses()
